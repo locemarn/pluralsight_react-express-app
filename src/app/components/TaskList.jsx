173e9b600edf57e0679bdc70246b932e070211ad
locemarn <marcelon27@gmail.com>
2020-04-03 11:23:18 -0300

    implemmenting Task Details Route / Mutatuing Data

--- src/app/components/TaskList.jsx
+++ src/app/components/TaskList.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { connect } from "react-redux";
 import { requestTaskCreation } from "../store/mutations";
+import { Link } from "react-router-dom";
 
 export const TaskList = ({ tasks, name, id, createNewTask }) => (
   <div>
     <h3>{name}</h3>
     {tasks.map(task => (
-      <div key={task.id}>{task.name}</div>
+      <Link to={`/task/${task.id}`} key={task.id}>
+        <div>{task.name}</div>
+      </Link>
     ))}
     <button onClick={() => createNewTask(id)}>Create</button>
   </div>
@@ -24,7 +27,6 @@ const mapStateToProps = (state, ownProps) => {
 const mapDispatchToProps = (dispatch, ownProps) => {
   return {
     createNewTask(id) {
-      console.log('Create: ', id)
       dispatch(requestTaskCreation(id))
     }
   }
